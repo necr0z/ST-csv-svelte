bc54d79d0f5ebbb9a4214fe5ed91ee70bdccccc6
necr0z <necroz1998@gmail.com>
2025-01-28 22:43:14 +0800

    backend tests done

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,13 @@
+// vite.config.ts
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		environment: 'jsdom', // ONLY for tests, dev mode unaffected
+		globals: true, // optional if you want describe/it globally
+		include: ['src/**/*.{test,spec}.{js,ts}'], // pick up test files
+		setupFiles: ['src/setupTest.ts'] // if you want global test setups
 	}
 });
